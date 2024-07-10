ee25061d5851a726f761d3193f352662b04631d8
celyes <celyes02@gmail.com>
2024-07-10 19:08:23 +0100

    feat: Add account control with logout functionality

--- src/components/account/AccountControl.vue
+++ src/components/account/AccountControl.vue
@@ -4,25 +4,31 @@ import { ref } from 'vue'
 import LoginForm from '@/components/account/LoginForm.vue'
 import LogoutForm from '@/components/account/LogoutForm.vue'
 import { useUserStore } from '@/stores/user'
+import UserSettings from '@/components/account/UserSettings.vue'
 
 const userStore = useUserStore()
-const loginOpened = ref(false)
+const isLoginOpened = ref(false)
 
 </script>
 
 <template>
-  <div>
-    <template v-if="!userStore.isLoggedIn">
-        <button
-          @click="loginOpened = !loginOpened"
-          class="border border-white text-white rounded-md px-3 py-2 text-md font-medium hover:bg-emerald-700 hover:border-transparent duration-100"
-          aria-current="page">
-          Login
-        </button>
-        <LoginForm :is-shown="loginOpened" @close="loginOpened = false" />
-    </template>
-    <LogoutForm v-else />
-  </div>
+    <div>
+        <template v-if="!userStore.isLoggedIn">
+            <button
+                @click="isLoginOpened = !isLoginOpened"
+                class="border border-white text-white rounded-md px-3 py-2 text-md font-medium hover:bg-emerald-700 hover:border-transparent duration-100"
+                aria-current="page">
+                Login
+            </button>
+            <LoginForm :is-shown="isLoginOpened" @close="isLoginOpened = false" />
+        </template>
+        <template v-else>
+            <div class="flex">
+                <LogoutForm />
+                <UserSettings />
+            </div>
+        </template>
+    </div>
 </template>
 
 <style scoped>
--- src/components/account/LogoutForm.vue
+++ src/components/account/LogoutForm.vue
@@ -1,13 +1,19 @@
 <template>
-  <!-- To be implemented -->
-  <button
-    type="button"
-    class="border border-white text-white rounded-md px-3 py-2 text-md font-medium hover:bg-emerald-700 hover:border-transparent duration-100"
-    aria-current="page">
-    Logout
-  </button>
+    <!-- To be implemented -->
+    <button
+        @click="logout"
+        type="button"
+        class="border border-white text-white rounded-md px-3 py-2 text-md font-medium hover:bg-emerald-700 hover:border-transparent duration-100"
+        aria-current="page">
+        Logout
+    </button>
 </template>
 
 <script setup lang="ts">
+import { useUserStore } from '@/stores/user'
 
+const userStore = useUserStore()
+const logout = () => {
+    userStore.logout()
+}
 </script>
